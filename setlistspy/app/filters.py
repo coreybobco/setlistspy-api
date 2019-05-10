6852515534c75a9ad2e385f043da6af4c8caf05c
Corey Bobco <corey.bobco@gmail.com>
2019-05-10 13:25:42 -0700

    changed default filter lookup expr to iexact, fixes #12

--- setlistspy/app/filters.py
+++ setlistspy/app/filters.py
@@ -1,39 +1,45 @@
 import rest_framework_filters as filters
-from rest_framework_filters.filters import BooleanFilter
+from rest_framework_filters.filters import BooleanFilter, CharFilter
 
 from setlistspy.app.models import Artist, DJ, Label, Setlist, Track, TrackPlay
 
 
 class ArtistFilter(filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='iexact')
 
     class Meta:
         model = Artist
         fields = {
-            'name': ['exact', 'in', 'startswith', 'icontains'],
+            'name': ['iexact', 'in', 'startswith', 'icontains'],
         }
 
 
 class LabelFilter(filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='iexact')
+    track_plays = filters.RelatedFilter('TrackPlayFilter', field_name='track_plays', queryset=TrackPlay.objects.all())
 
     class Meta:
         model = Label
         fields = {
-            'name': ['exact', 'in', 'startswith', 'icontains'],
-            'track_plays__setlist__dj__name': ['exact', 'in', 'startswith', 'icontains'],
+            'name': ['iexact', 'in', 'startswith', 'icontains'],
         }
 
 
 class DJFilter(filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='iexact')
+    setlists = filters.RelatedFilter('SetlistFilter', field_name='setlists', queryset=Setlist.objects.all())
 
     class Meta:
         model = DJ
         fields = {
-            'name': ['exact', 'in', 'startswith', 'icontains'],
-            'setlists__track_plays__label__name': ['exact', 'in', 'startswith', 'icontains'],
+            'name': ['iexact', 'in', 'startswith', 'icontains'],
         }
 
 
 class SetlistFilter(filters.FilterSet):
+    title = CharFilter(field_name='title', lookup_expr='iexact')
+    dj = filters.RelatedFilter('DJFilter', field_name='dj', queryset=DJ.objects.all())
+    track_plays = filters.RelatedFilter('TrackPlayFilter', field_name='track_plays', queryset=TrackPlay.objects.all())
     empty = BooleanFilter(field_name='empty', method='filter_empty')
 
     def filter_empty(self, qs, name, value):
@@ -42,36 +48,33 @@ class SetlistFilter(filters.FilterSet):
     class Meta:
         model = Setlist
         fields = {
-            'dj__name': ['exact', 'in', 'startswith', 'icontains'],
-            'title': ['exact', 'in', 'startswith', 'icontains'],
+            # 'dj__name': ['iexact', 'in', 'startswith', 'icontains'],
+            'title': ['iexact', 'in', 'startswith', 'icontains'],
             'mixesdb_id': ['exact', 'in'],
             'b2b': ['exact'],
         }
 
 
 class TrackFilter(filters.FilterSet):
+    title = CharFilter(field_name='title', lookup_expr='iexact')
+    artist = filters.RelatedFilter('ArtistFilter', field_name='artist', queryset=Artist.objects.all())
+    setlists = filters.RelatedFilter('SetlistFilter', field_name='setlists', queryset=Setlist.objects.all())
+    plays = filters.RelatedFilter('TrackPlayFilter', field_name='plays', queryset=TrackPlay.objects.all())
 
     class Meta:
         model = Track
         fields = {
-            'artist__name': ['exact', 'in', 'startswith', 'icontains'],
-            'title': ['exact', 'in', 'startswith', 'icontains'],
-            'setlists__title': ['exact', 'in', 'startswith', 'icontains'],
-            'setlists__dj__name': ['exact', 'in', 'startswith', 'icontains'],
-            'plays__label__name': ['exact', 'in', 'startswith', 'icontains'],
-            'plays__set_order': ['exact', 'in'],
+            'title': ['iexact', 'in', 'startswith', 'icontains'],
         }
 
 
 class TrackPlayFilter(filters.FilterSet):
+    label = filters.RelatedFilter('LabelFilter', field_name='label', queryset=Label.objects.all())
+    track = filters.RelatedFilter('TrackFilter', field_name='track', queryset=Track.objects.all())
+    setlist = filters.RelatedFilter('SetlistFilter', field_name='setlist', queryset=Setlist.objects.all())
 
     class Meta:
         model = TrackPlay
         fields = {
-            'label__name': ['exact', 'in', 'startswith', 'icontains'],
-            'track__artist__name': ['exact', 'in', 'startswith', 'icontains'],
-            'track__title': ['exact', 'in', 'startswith', 'icontains'],
-            'setlist__title': ['exact', 'in', 'startswith', 'icontains'],
-            'setlist__dj__name': ['exact', 'in', 'startswith', 'icontains'],
             'set_order': ['exact', 'in']
         }
